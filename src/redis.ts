3c3bb8467ad54e27d18a126834bb34138b821e6e
4cozm <17328rm@gmail.com>
2026-02-06 20:42:09 +0900

    redis 연결 폴백 추가

--- src/redis.ts
+++ src/redis.ts
@@ -1,11 +1,18 @@
 import { Redis } from "ioredis";
 import { config } from "./config.js";
 
+const RECONNECT_INTERVAL_MS = 10_000;
+
 let client: Redis | null = null;
 
 export function getRedis(): Redis {
   if (!client) {
-    client = new Redis(config.REDIS_URL, { maxRetriesPerRequest: null });
+    client = new Redis(config.REDIS_URL, {
+      maxRetriesPerRequest: null,
+      retryStrategy(times: number): number {
+        return RECONNECT_INTERVAL_MS;
+      },
+    });
   }
   return client;
 }
